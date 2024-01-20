c0a4ec819339173d7bee40444723407b13e0c1e8
Shubhayu Dasgupta <MY_NAME@example.com>
2024-01-20 13:12:20 +0530

    changes in cors

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
 const express= require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const dbConnection = require('./database/database');
 const homeRouter = require('./Routes/homeRoutes');
 require('dotenv').config();
 
-
-
-const app = express();
 const PORT = process.env.PORT;
 
+const app = express();
 
+
+
+app.use(cors());
+app.options('*', cors()); 
 app.use(bodyParser.json());
 app.use(homeRouter);
-
-
-
-
-
-
 
 app.listen(PORT, ()=>{
     console.log('SERVER started on port',PORT)
